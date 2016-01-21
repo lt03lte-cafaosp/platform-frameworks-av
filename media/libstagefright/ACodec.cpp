88afcd32dafaef3722246ffb1b4c217706ebd048
Christopher R. Palmer <crpalmer@gmail.com>
2016-01-20 20:31:42 -0500

    av: "ACodec: update native window crop rectangle" depends on QCOM
    Change-Id: If0cc81468ab7ee8fa0ec374a9f23e4004e7cb212

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -1028,6 +1028,7 @@ status_t ACodec::setupNativeWindowSizeFormatAndUsage(
             mRotationDegrees,
             usage,
             reconnect);
+#ifdef QCOM_HARDWARE
     if (err == OK) {
         OMX_CONFIG_RECTTYPE rect;
         InitOMXParams(&rect);
@@ -1045,6 +1046,7 @@ status_t ACodec::setupNativeWindowSizeFormatAndUsage(
             err = native_window_set_crop(nativeWindow, &crop);
         }
     }
+#endif
     return err;
 }
 
